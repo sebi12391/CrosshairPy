d5edbdaeb39965de3eaeaf829b63b7234d37eba9
Sebastian Krawiec <sebi.krawiec@gmail.com>
2023-09-25 13:00:18 -0500

    Update #1
    1. Added customizable xy coordinate offsets to allow the user to offset the crosshair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pyautogui
+import ctypes
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget
@@ -14,7 +15,9 @@ DEFAULT_SETTINGS = {
     "RESIZE_TO_WIDTH": 50,
     "RESIZE_TO_HEIGHT": 50,
     "TIMER_INTERVAL_MS": 1,
-    "FOLLOW_MOUSE": True
+    "FOLLOW_MOUSE": True,
+    "OFFSET_X": 0,  # Offset from the center or mouse in the X-axis
+    "OFFSET_Y": 0   # Offset from the center or mouse in the Y-axis
 }
 
 # Check if the settings file exists, if not, create it with default values
@@ -27,6 +30,8 @@ if not os.path.isfile(SETTINGS_FILE):
 with open(SETTINGS_FILE, "r") as settings_file:
     SETTINGS = json.load(settings_file)
 
+# Set the title of the command prompt window to "Crosshair Controller"
+ctypes.windll.kernel32.SetConsoleTitleW("Crosshair Controller")
 
 class Crosshair(QWidget):
     def __init__(self):
@@ -35,15 +40,15 @@ class Crosshair(QWidget):
 
     def initUI(self):
         # Set window properties
-        self.setWindowTitle("Crosshair Overlay")  # Set the window title
+        self.setWindowTitle("Crosshair Overlay")  # Set the crosshair window title
         self.setWindowFlags(
             Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint | Qt.WindowTransparentForInput
         )
-
+        
         # Get the screen resolution using screeninfo
-        monitor = get_monitors()[0]  # Assuming primary monitor is the first one
+        monitor = get_monitors()[0]  # Assuming the user wants to use the primary monitor
         screen_width, screen_height = monitor.width, monitor.height
-
+        
         self.setGeometry(0, 0, screen_width, screen_height)
         self.setAttribute(Qt.WA_TranslucentBackground)
 
@@ -51,8 +56,7 @@ class Crosshair(QWidget):
         self.crosshair_label = QLabel(self)
         original_pixmap = QPixmap(SETTINGS["CROSSHAIR_IMAGE_NAME"])
         if SETTINGS["RESIZE_IMAGE"]:
-            pixmap = original_pixmap.scaled(SETTINGS["RESIZE_TO_WIDTH"], SETTINGS["RESIZE_TO_HEIGHT"],
-                                            Qt.KeepAspectRatio, Qt.SmoothTransformation)
+            pixmap = original_pixmap.scaled(SETTINGS["RESIZE_TO_WIDTH"], SETTINGS["RESIZE_TO_HEIGHT"], Qt.KeepAspectRatio, Qt.SmoothTransformation)
         else:
             pixmap = original_pixmap
         self.crosshair_label.setPixmap(pixmap)
@@ -72,24 +76,25 @@ class Crosshair(QWidget):
               f"Resize to Width: {SETTINGS['RESIZE_TO_WIDTH']}\n"
               f"Resize to Height: {SETTINGS['RESIZE_TO_HEIGHT']}\n"
               f"Timer Interval (ms): {SETTINGS['TIMER_INTERVAL_MS']}\n"
-              f"Follow Mouse: {SETTINGS['FOLLOW_MOUSE']}\n")
+              f"Follow Mouse: {SETTINGS['FOLLOW_MOUSE']}\n"
+              f"Offset X: {SETTINGS['OFFSET_X']}\n"
+              f"Offset Y: {SETTINGS['OFFSET_Y']}\n")
 
     def updateCrosshairPosition(self):
         if SETTINGS["FOLLOW_MOUSE"]:
             # Get the current cursor position
             cursor_x, cursor_y = pyautogui.position()
 
-            # Calculate the crosshair position
-            crosshair_x = cursor_x - self.crosshair_label.width() // 2
-            crosshair_y = cursor_y - self.crosshair_label.height() // 2
+            # Calculate the crosshair position with offset
+            crosshair_x = cursor_x - self.crosshair_label.width() // 2 + SETTINGS["OFFSET_X"]
+            crosshair_y = cursor_y - self.crosshair_label.height() // 2 + SETTINGS["OFFSET_Y"]
         else:
-            # Display the crosshair in the center of the screen
-            crosshair_x = (self.width() - self.crosshair_label.width()) // 2
-            crosshair_y = (self.height() - self.crosshair_label.height()) // 2
+            # Display the crosshair in the center of the screen with offset
+            crosshair_x = (self.width() - self.crosshair_label.width()) // 2 + SETTINGS["OFFSET_X"]
+            crosshair_y = (self.height() - self.crosshair_label.height()) // 2 + SETTINGS["OFFSET_Y"]
 
         # Move the crosshair label to the calculated position
         self.crosshair_label.move(crosshair_x, crosshair_y)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
